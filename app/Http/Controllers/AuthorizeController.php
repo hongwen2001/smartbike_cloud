e3910eb545a692aab06a9f138128bb96962831e0
DESKTOP-B9U2606\user <d1094162060@gm.lhu.edu.tw>
2022-11-23 17:08:11 +0800

    Revert "1234"
    This reverts commit 1a1c07b8fad710360b03472327c6a9c6fe34ebac.

--- app/Http/Controllers/AuthorizeController.php
+++ app/Http/Controllers/AuthorizeController.php
@@ -12,26 +12,42 @@ use Laravel\Passport\Http\Controllers\ClientController;
 use function Livewire\str;
 use function Symfony\Component\String\u;
 
+/*    隨著IP不同這裡需要更改
+ *
+ * */
+
 class AuthorizeController extends ClientController
 {
     //
     public function redirect(Request $request)
     {
         $user = Auth::user();
-
         global $response;
 
-        $request = $request->replace(['name' => Auth::user()->account, 'redirect' => 'http://140.131.3.97:8080/authorize2/callback']);
-        $sec = $this->store($request);
-        DB::table('users')->where('id', '=', $user->id)->update(['secret' => $sec->secret, 'client_id' => $sec->id]);
-        $response = http_build_query(['client_id' => $sec->id, 'redirect_uri' => 'http://140.131.3.97/authorize2/callback', 'response_type' => 'code', 'scope' => '', 'clients' => $sec]);
+        if ($user->secret == null) {
+<<<<<<< HEAD
+            $request = $request->replace(['name' => Auth::user()->account, 'redirect' => 'http://140.131.3.97:8080/authorize2/callback']);
+            $sec = $this->store($request);
+            DB::table('users')->where('id', '=', $user->id)->update(['secret' => $sec->secret, 'client_id' => $sec->id]);
+            $response = http_build_query(['client_id' => $sec->id, 'redirect_uri' => 'http://140.131.3.97:8080/authorize2/callback', 'response_type' => 'code', 'scope' => '']);
+        } else {
+            $response = http_build_query(['client_id' => $user->client_id, 'redirect_uri' => 'http://140.131.3.97:8080/authorize2/callback', 'response_type' => 'code', 'scope' => '']);
+        }
         return redirect('http://140.131.3.97:8080/oauth/authorize?' . $response);
+=======
+            $request = $request->replace(['name' => Auth::user()->account, 'redirect' => 'http://192.168.143.112:8080/authorize2/callback']);
+            $sec = $this->store($request);
+            DB::table('users')->where('id', '=', $user->id)->update(['secret' => $sec->secret, 'client_id' => $sec->id]);
+            $response = http_build_query(['client_id' => $sec->id, 'redirect_uri' => 'http://192.168.143.112:8080/authorize2/callback', 'response_type' => 'code', 'scope' => '']);
+        } else {
+            $response = http_build_query(['client_id' => $user->client_id, 'redirect_uri' => 'http://192.168.143.112:8080/authorize2/callback', 'response_type' => 'code', 'scope' => '']);
+        }
+        return redirect('http://192.168.143.112:8080/oauth/authorize?' . $response);
+>>>>>>> origin/master
+        //        $response=http_build_query(['client_id'=>'1','redirect_uri'=>'http://127.0.0.1:8080/authorize2/callback','response_type'=>'code','scope'=>'']);
 
 
-//        $response=http_build_query(['client_id'=>'1','redirect_uri'=>'http://127.0.0.1:8080/authorize2/callback','response_type'=>'code','scope'=>'']);
-
-
-//        return $sec;
+        //        return $sec;
     }
 
     public function again_Authorize(Request $request)
@@ -47,17 +63,15 @@ class AuthorizeController extends ClientController
     {
         $user = Auth::user();
         if ($request->has('code')) {
-            $user_data = DB::table('users')->where('id', '=', $user->id)->first();
-            return redirect()->to('/dashboard?code=' . $request->code);
+            return redirect()->to('/dashboard?code=' . $request->code . '&client_id=' . $user->client_id.'&secret='.$user->secret.'&id='.$user->id);
         }
         if ($request->has('error')) {
-            $user_data = DB::table('users')->where('id', '=', $user->id);
-            $client_id=$user_data->first()->client_id;
-            $this->destroy($request, str($client_id));
-            $user_data->update(['secret' => null, 'client_id' => null]);
-//            return $this->redirect()->to("/dashboard");
-            return "倒置";
-        }
 
+            $client_id = $user->client_id;
+            $this->destroy($request, str($client_id));
+            $user->update(['secret' => '', 'client_id' => '']);
+            //            return $this->redirect()->to("/dashboard");
+            return response()->json(['status' => 1, 'message' => "error"]);
+        }
     }
 }
